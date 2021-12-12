af435e4805fe41270518878c09b64a26bc26164b
maciek <erbelionx@gmail.com>
2021-12-12 22:12:49 +0100

    db init

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,9 +1,10 @@
 package Main;
 
+import Controllers.DB;
+
 import java.awt.*;
 
 public class Main {
-
 
     public static void main(String[] args){
         EventQueue.invokeLater(new Runnable() {
