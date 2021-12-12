7f8b5e9814835e506bcf679beb4cfb67d2dc016a
maciek <erbelionx@gmail.com>
2021-12-12 22:25:19 +0100

    test

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,16 +1,15 @@
 package Main;
 
-import Controllers.DB;
-
 import java.awt.*;
-
+//jrieojfioejfioewjio
 public class Main {
+
 
     public static void main(String[] args){
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new View.UzytkownikWidok();
-            }
+            }//
         });
     }
 
