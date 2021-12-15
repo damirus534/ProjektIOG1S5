8988e285f0e026e70c60bf6328e2be2270acce31
Jacek <jacekwegr@gmail.com>
2021-12-15 12:13:42 +0100

    Update Main.java

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args){
         EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new View.UzytkownikWidok();
+                new View.Okno();
             }
         });
     }
